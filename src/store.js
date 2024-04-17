ef8e48df573099f5b6421b9b93af3d198b15ad1f
asma.gaini@yahoo.com <asma.gaini@yahoo.com>
2024-04-17 13:56:57 +0330

    install redux pakage(npm i redux),and import in index.js and create store in store.js and create dispatch like reducer

--- src/store.js
+++ src/store.js
@@ -1,3 +1,6 @@
+import { createStore } from "redux";
+//ma tu inja function mon ru export nemikonim
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -14,7 +17,9 @@ function reducer(state = initialState, action) {
       // LATER
       return {
         ...state,
-        loan: action.payload,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
       };
     case "account/payLoan":
       return {
@@ -27,3 +32,19 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+//ma store ru misazin ba reducer
+const store = createStore(reducer);
+//hala b in store mitunim eghdamat dispatch ru ersal konim:
+store.dispatch({ type: "account/deposit", payload: 500 });
+store.dispatch({ type: "account/withdrawal", payload: 200 });
+console.log(store.getState());
+
+//on LATER bala mikhaym chand ta dade ru set konim
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: "Buy a car" },
+});
+console.log(store.getState());
+
+store.dispatch({ type: "account/payLoan" });
+console.log(store.getState());
