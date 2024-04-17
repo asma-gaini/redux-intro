7f6adb34a527e2222e17df3349d1a0560714f0bb
asma.gaini@yahoo.com <asma.gaini@yahoo.com>
2024-04-17 18:01:24 +0330

    create customer reducer and combine reducers in root reducer and use this

--- src/store.js
+++ src/store.js
@@ -1,12 +1,19 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 //ma tu inja function mon ru export nemikonim
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
-function reducer(state = initialState, action) {
+
+const initialStateCustomer = {
+  fullName: "",
+  nationalId: "",
+  createdAt: "",
+};
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
@@ -32,8 +39,32 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalId: action.payload.nationalId,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
+
+// on reducer yi k b store mifrestim b onvan rishe darnazar gerefte mishe pas ma miyaym hame reducer hamon ro
+// dar ye reducer rishe tarkibeshon mikonim va ono vared store mikonim
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
 //ma store ru misazin ba reducer
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 //hala b in store mitunim eghdamat dispatch ru ersal konim:
 // store.dispatch({ type: "account/deposit", payload: 500 });
 // store.dispatch({ type: "account/withdrawal", payload: 200 });
@@ -68,4 +99,18 @@ store.dispatch(withdrawal(200));
 store.dispatch(requestLoan(1000, "buy a car"));
 store.dispatch(payLoan());
 
+console.log(store.getState());
+
+function createCustomer(fullName, nationalId) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalId, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return { type: "customer/updateName", payload: fullName };
+}
+
+store.dispatch(createCustomer("asma gaini", "0371560004"));
 console.log(store.getState());
